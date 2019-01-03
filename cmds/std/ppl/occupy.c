aee0506bd055542f151b7f5403ffaa47d3eede53
oiuv <i@oiuv.cn>
2019-01-03 16:07:00 +0000

    修正occupy问题

--- cmds/std/ppl/occupy.c
+++ cmds/std/ppl/occupy.c
@@ -6,11 +6,11 @@
  * Date   : 2005-02-27
  * Note   : 佔領指令
  * Update :
- *  o 2005-00-00  
+ *  o 2005-00-00
  *
  -----------------------------------------
  */
- 
+
 #include <ansi.h>
 #include <feature.h>
 #include <location.h>
@@ -42,9 +42,9 @@ void confirm_occupy_new_city(object me, mapping info, string arg)
 		tell(me, pnoun(2, me)+"取消了佔領程序。\n");
 		return me->finish_input();
 	}
-	
+
 	CITY_D->occupy_new_city(me, info["id"], info["name"], info["moneyunit"]);
-	
+
 	me->finish_input();
 }
 
@@ -55,7 +55,7 @@ void confirm_occupy_section_city(object me, string arg)
 	int needed_flourish;
 	int total_num;
 	string city;
-	
+
 	if( !arg || (lower_case(arg) != "y" && lower_case(arg) != "yes") )
 	{
 		tell(me, pnoun(2, me)+"取消了佔領程序。\n");
@@ -63,7 +63,7 @@ void confirm_occupy_section_city(object me, string arg)
 	}
 
 	city = query("city", me);
-	
+
 	total_num = CITY_D->query_city_num(city);
 	needed_flourish = 50000 + (total_num-1)*5000;
 
@@ -74,7 +74,7 @@ void confirm_occupy_section_city(object me, string arg)
 	}
 
 	CITY_D->occupy_section_city(me);
-	
+
 	me->finish_input();
 }
 
@@ -94,7 +94,7 @@ void input_money_unit(object me, mapping info, string arg)
 
 	tell(me, NOR CYN"\n第四步："HIC"以上設定是否正確無誤？(Yes/No)"NOR CYN"："NOR);
 	input_to((: confirm_occupy_new_city, me, info :));
-}		
+}
 
 void input_color_id(object me, mapping info, string arg)
 {
@@ -124,7 +124,7 @@ void input_color_name(object me, mapping info, string arg)
 	}
 
 	if( query("encode/gb", me) )
-		arg = B2G(arg);
+		arg = G2B(arg);
 
 	arg = ansi(arg);
 	arg = replace_string(arg, " ", "");
@@ -141,7 +141,7 @@ private void do_command(object me, string arg)
 	string mycity;
 	object env = environment(me);
 	array loc = query_temp("location", me);
-	
+
 	if( arg != "city" )
 		return tell(me, pnoun(2, me)+"想要佔領什麼？\n");
 
@@ -150,19 +150,19 @@ private void do_command(object me, string arg)
 
 	if( CITY_D->query_city_info(loc[CITY], "no_occupy") )
 		return tell(me, "目前無法佔領此城市。\n");
-	
+
 	mycity = query("city", me);
 
 	if( !mycity )
 	{
 		mapping info = allocate_mapping(0);
-			
+/*
 		if( query("total_online_time", me) < 30*24*60*60 )
 			return tell(me, pnoun(2, me)+"的上線時間至少必須超過一個月才能佔領新城市。\n");
-			
+*/
 		if( count(MONEY_D->query_all_cash(me->query_id(1), MONEY_D->query_default_money_unit()), "<", "10000000000") )
 			return tell(me, pnoun(2, me)+"的必須擁有至少 $"+MONEY_D->query_default_money_unit()+" 100 億的現金才能佔領新城市。\n");
-			
+
 		CHANNEL_D->channel_broadcast("news", me->query_idname()+"正在考慮於"+CITY_D->query_city_idname(loc[CITY])+"建立全新城市");
 		tell(me, WARNING+NOR CYN"\n第一步："HIC"請替"+pnoun(2, me)+"的新城市取一個彩色中文名稱(1 - 5 個中文字)\n"NOR CYN"："NOR);
 		input_to((: input_color_name, me, info :));
