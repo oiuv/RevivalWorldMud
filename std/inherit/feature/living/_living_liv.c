0bb3058c119600185946748bdeff8bef5186d5a0
oiuv <i@oiuv.cn>
2019-01-09 13:21:08 +0000

    增加负重上限

--- std/inherit/feature/living/_living_liv.c
+++ std/inherit/feature/living/_living_liv.c
@@ -6,7 +6,7 @@
  * Date   : 2001-12-17
  * Note   : 生物繼承檔
  * Update :
- *  o 2000-00-00  
+ *  o 2000-00-00
  *
  -----------------------------------------
  */
@@ -19,13 +19,13 @@ nomask void startup_living()
 {
 	if( !userp(this_object()) )
 		CHAR_D->create_npc(this_object());
-	
+
 	set_heart_beat(10);
 }
 
 nomask int is_living()
 {
-	return 1;	
+	return 1;
 }
 
 int query_inventory_slot()
@@ -37,21 +37,21 @@ int query_inventory_slot()
 nomask int query_loading()
 {
 	int loading, mass;
-	
+
 	foreach(object ob in all_inventory(this_object()))
 	{
 		mass = query("mass", ob);
-		
+
 		if( mass > 0 )
 			loading += to_int(count(mass, "*", query_temp("amount", ob)||1));
 	}
-	
-	return loading;	
+
+	return loading;
 }
 
 nomask int query_max_loading()
 {
-	return 1000*(this_object()->query_str() + this_object()->query_condition_buff(BUFF_LOADING_MAX) + this_object()->query_equipment_buff(BUFF_LOADING_MAX));
+	return 1000*(5*(this_object()->query_str()) + this_object()->query_condition_buff(BUFF_LOADING_MAX) + this_object()->query_equipment_buff(BUFF_LOADING_MAX));
 }
 
 varargs nomask int get_object(object ob, mixed amount)
@@ -73,11 +73,11 @@ varargs nomask int get_object(object ob, mixed amount)
 nomask int available_get(object ob, mixed amount)
 {
 	int mass = query("mass", ob);
-	
+
 	// 若超過負重則 return 0
 	if( mass == -1 || count(count(amount||1,"*",mass),">", query_max_loading()-query_loading()) )
 		return 0;
-	
+
 	return 1;
 }
 
@@ -91,19 +91,19 @@ nomask void faint()
 	COMBAT_D->stop_fight(this_object());
 
 	msg("\e[1;31m$ME\e[1;31m突然眼前一堆小星星，昏倒了。\n\e[m", this_object(), 0, 1);
-	
+
 	set("faint", 1);
 	set_temp("disable/msg", 1);
 	set_temp("disable/cmd", 1);
 }
 
-// 甦醒	
+// 甦醒
 nomask void wakeup()
 {
 	delete("faint");
 	delete_temp("disable/msg");
 	delete_temp("disable/cmd");
-	
+
 	msg("$ME逐漸地恢復了意識。\n", this_object(), 0, 1);
 	this_object()->show_msg_buffer();
 	this_object()->show_prompt();
